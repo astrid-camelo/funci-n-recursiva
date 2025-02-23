e0ea09bd15609b67d145ba77195f5edde450177f
astrid-camelo <yulitzatorres0216@gmail.com>
2025-02-23 18:13:23 -0500

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -16,21 +16,19 @@
     echo "El resultado de la Resta recursiva: " . $client->operacion(9, 3, 'resta') . "<br>"; 
     echo "El resultado de la Multiplicación recursiva: " . $client->operacion(4, 7, 'multiplicacion') . "<br>"; 
     echo "El resultado de la División recursiva: " . $client->operacion(12, 5, 'division') . "<br>"; 
-    echo "<br>";
-    echo "El producto es: " .$client->getProduct();
+    //echo "El producto es: " .$client->getProduct();
     echo "<br>";
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nombre = $_POST['nombre'];
+        $usuario = $_POST['usuario'];
         $clave = $_POST['clave'];
-        $resultado = $client->validarUsuario($nombre, $clave);
+        $resultado = $client->validarUsuario($usuario, $clave);
         echo $resultado; 
     }
 ?>
 <form method="POST" action=""> <br>
-    <label for="nombre">nombre:</label>
-    <input type="text" name="nombre" id="nombre" required><br>
+    <label for="usuario">usuario:</label>
+    <input type="text" name="usuario" id="usuario" required><br>
     <label for="clave">Clave:</label>
     <input type="password" name="clave" id="clave" required><br>
     <input type="submit" value="Iniciar sesión">
 </form>
-
